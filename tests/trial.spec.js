07775bca919d0d0b399ba8693d81a51333074b35
krishnapriya27051997-glitch <krishnapriya27051997-glitch>
2026-06-04 17:29:50 +0530

    initial

---
+++ tests/trial.spec.js
@@ -0,0 +1,20 @@
+import test from '@playwright/test'
+
+test('Myntra', async ({ page }) => {
+
+    await page.goto('https://www.myntra.com/boy-tshirts');
+    const discount_product_price = await page.locator('//li[@class="product-base"]/descendant::span[@class="product-discountedPrice"]').allTextContents();
+
+    const cleaned_prices = discount_product_price.map(price => price.replace(/[^\d]/g, ''));
+
+    console.log(cleaned_prices);
+
+   const minPrice = Math.min(...cleaned_prices.map(Number));
+
+   console.log(minPrice);
+   const minpricebrand =  
+
+
+
+});
+
