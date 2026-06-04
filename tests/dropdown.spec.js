7734d2b460872db72490a7b07851cb63ac02a3aa
krishnapriya27051997-glitch <krishnapriya27051997-glitch>
2026-06-04 18:36:07 +0530

    added line

--- tests/dropdown.spec.js
+++ tests/dropdown.spec.js
@@ -2,5 +2,5 @@ import {test} from '@playwright/test';
 test ('https://demoqa.com/webtables'),async ({page}) => {    
 const table = await page.locator('<tbody><tr>');
 console.log(table)
-
-};
+console.log (await table.count())
+}
